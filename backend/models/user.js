0a181e600185da3b018d333617640e56ff63d68c
sippu0407 <suryabhan.singh.0407@gmail.com>
2022-07-16 11:46:06 +0530

    day 11

--- backend/models/user.js
+++ backend/models/user.js
@@ -22,6 +22,12 @@ const userSchema = mongoose.Schema({
     required: true,
     default: false,
   },
+  role: {
+    type: String,
+    required: true,
+    default: "user",
+    enum: ["admin", "user"],
+  },
 });
 
 userSchema.pre("save", async function (next) {
@@ -31,5 +37,10 @@ userSchema.pre("save", async function (next) {
 
   next();
 });
+
+userSchema.methods.comparePassword = async function (password) {
+  const result = await bcrypt.compare(password, this.password);
+  return result;
+};
 
 module.exports = mongoose.model("User", userSchema);
---
+++ backend/routes/actor.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const {
+  createActor,
+  updateActor,
+  removeActor,
+  searchActor,
+  getLatestActors,
+  getSingleActor,
+} = require("../controllers/actor");
+const { isAuth, isAdmin } = require("../middlewares/auth");
+const { uploadImage } = require("../middlewares/multer");
+const { actorInfoValidator, validate } = require("../middlewares/validator");
+const router = express.Router();
+
+router.post(
+  "/create",
+  isAuth,
+  isAdmin,
+  uploadImage.single("avatar"),
+  actorInfoValidator,
+  validate,
+  createActor
+);
+
+router.post(
+  "/update/:actorId",
+  isAuth,
+  isAdmin,
+  uploadImage.single("avatar"),
+  actorInfoValidator,
+  validate,
+  updateActor
+);
+
+router.delete("/:actorId", isAuth, isAdmin, removeActor);
+router.get("/search", isAuth, isAdmin, searchActor);
+router.get("/latest-uploads", isAuth, isAdmin, getLatestActors);
+router.get("/single/:id", getSingleActor);
+module.exports = router;
